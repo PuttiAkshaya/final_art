2bbd7a1287987d7247f5b74121f8ca10f0adcfa7
PuttiAkshaya <2400032442@kluniversity.in>
2026-04-28 03:07:49 +0530

    delte

--- artbackend/src/main/java/com/klu/repository/ExhibitionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klu.repository;
-
-import com.klu.model.Exhibition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
-}
